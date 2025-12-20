f62cea5c1ce405f07d1663c3847298782feea241
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-12-20 16:18:22 +0500

    Update journal_repository.dart

--- lib/features/journal/domain/repositories/journal_repository.dart
+++ lib/features/journal/domain/repositories/journal_repository.dart
@@ -3,6 +3,7 @@ import '../../../donation/domain/entities/donation.dart';
 /// Repository interface for journal (donations history) operations.
 abstract class JournalRepository {
   /// Get all donations from history.
-  Future<List<Donation>> getDonations();
+  /// [forceRefresh] - if true, bypass cache and fetch from server
+  Future<List<Donation>> getDonations({bool forceRefresh = false});
 }
 
