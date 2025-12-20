a0f7f6fa453efa29eb87750a84b139b655ed5372
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-12-20 16:16:42 +0500

    Update admin_providers.dart

--- lib/features/admin/presentation/providers/admin_providers.dart
+++ lib/features/admin/presentation/providers/admin_providers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/providers/google_sheets_provider.dart';
+import '../../../../core/providers/cache_provider.dart';
 import '../../data/repositories/admin_repository_impl.dart';
 import '../../domain/repositories/admin_repository.dart';
 import '../../domain/usecases/create_goal_usecase.dart';
@@ -7,11 +8,13 @@ import '../../domain/usecases/update_goal_usecase.dart';
 import '../../domain/usecases/delete_goal_usecase.dart';
 import '../../domain/usecases/delete_donation_usecase.dart';
 import '../../domain/usecases/delete_user_usecase.dart';
+import '../../domain/usecases/update_donation_status_usecase.dart';
 
 /// Provider for admin repository.
 final adminRepositoryProvider = Provider<AdminRepository>((ref) {
   final sheetsService = ref.watch(googleSheetsServiceProvider);
-  return AdminRepositoryImpl(sheetsService);
+  final cacheService = ref.watch(cacheServiceProvider);
+  return AdminRepositoryImpl(sheetsService, cacheService);
 });
 
 /// Provider for create goal use case.
@@ -42,5 +45,11 @@ final deleteDonationUseCaseProvider = Provider<DeleteDonationUseCase>((ref) {
 final deleteUserUseCaseProvider = Provider<DeleteUserUseCase>((ref) {
   final repository = ref.watch(adminRepositoryProvider);
   return DeleteUserUseCase(repository);
+});
+
+/// Provider for update donation status use case.
+final updateDonationStatusUseCaseProvider = Provider((ref) {
+  final repository = ref.watch(adminRepositoryProvider);
+  return UpdateDonationStatusUseCase(repository);
 });
 
