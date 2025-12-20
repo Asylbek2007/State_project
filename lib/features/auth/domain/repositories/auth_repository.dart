9fbfacb2118812af454b2f9fc3dc431d145775ce
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-12-20 16:16:58 +0500

    Update auth_repository.dart

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,14 +3,13 @@ import 'package:donation_app/features/registration/domain/entities/user.dart';
 
 /// Repository interface for authentication operations.
 abstract class AuthRepository {
-  /// Login user by checking credentials in Google Sheets.
+  /// Login user with email and password.
   ///
   /// Returns [User] if credentials match.
   /// Throws [Failure] if login fails.
   Future<User> loginUser(
-    String fullName,
-    String surname,
-    String studyGroup,
+    String email,
+    String password,
   );
 }
 
